f4f6d53dc29ffacc833937d91ea970fcfe57a5c6
Nirsch95 <castromanzano95@gmail.com>
2023-06-28 14:30:18 -0500

    First Backend Commit: Base project

---
+++ backend/src/test/java/com/nocountry/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
