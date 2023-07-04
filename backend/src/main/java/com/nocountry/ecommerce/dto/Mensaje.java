bedaceed9be5178bce51c628e8175931c029bb32
osdan97 <daniejurade@gmail.com>
2023-07-03 22:57:29 -0500

    creando dtoMensaje para category

--- backend/src/main/java/com/nocountry/ecommerce/dto/Mensaje.java
+++ backend/src/main/java/com/nocountry/ecommerce/dto/Mensaje.java
@@ -1,2 +1,16 @@
-package com.nocountry.ecommerce.dto;public class Mensaje {
+package com.nocountry.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mensaje {
+
+    private String mensaje;
+
 }
