cde8c08f4e07779f28e6684bbaace1802adf7f06
Metaldev-06 <fernydiaz62@gmail.com>
2023-07-04 22:01:02 -0300

    Add fixed header

--- frontend/src/app/ecommerce/layout/header/header.component.ts
+++ frontend/src/app/ecommerce/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   value: string | undefined;
+  headerFixed = signal<boolean>(false);
+
+  @HostListener('window:scroll', ['$event']) activeFixedHeader() {
+    if (window.scrollY >= 119) {
+      this.headerFixed.set(true);
+      console.log(this.headerFixed());
+    } else {
+      this.headerFixed.set(false);
+      console.log(this.headerFixed());
+    }
+  }
 }
