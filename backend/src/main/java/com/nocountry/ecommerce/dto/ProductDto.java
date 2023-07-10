4e370f06c75cc022824eded5d9a59511ab69b23d
Nirsch <37886668+Nirsch95@users.noreply.github.com>
2023-07-10 09:56:54 -0500

    Update ProductDto.java

--- backend/src/main/java/com/nocountry/ecommerce/dto/ProductDto.java
+++ backend/src/main/java/com/nocountry/ecommerce/dto/ProductDto.java
@@ -13,6 +13,7 @@ public class ProductDto {
     private String name;
     private String description;
     private Integer stock;
+    private Integer minStock;
     private String image;
     private Double price;
     private Double weight;
