f637c498bca0f852855fb0ebf42377b8b7563524
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-07-13 12:21:15 -0400

    Actualizando para recuperacion de password User Admin en SecurityConfig

--- backend/src/main/java/com/nocountry/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/nocountry/ecommerce/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST,"api/inventory/create",
                         "api/category/create",
                         "api/products",
+                        "/api/authentication/forgot-password",
                         "api/transaction/inventory")
                 .hasRole(Role.ADMIN.name())
 
