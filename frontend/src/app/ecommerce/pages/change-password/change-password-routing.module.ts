cc925e165e7eaed83af41384993ae2616f08f78a
Metaldev-06 <fernydiaz62@gmail.com>
2023-07-14 14:44:45 -0300

    Add: change password page

--- frontend/src/app/ecommerce/pages/change-password/change-password-routing.module.ts
+++ frontend/src/app/ecommerce/pages/change-password/change-password-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './change-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ChangePasswordComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ChangePasswordRoutingModule { }
+export class ChangePasswordRoutingModule {}
