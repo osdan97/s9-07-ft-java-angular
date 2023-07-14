866f9f7b0677d5a6436210da43945ec5f440dfd9
Metaldev-06 <fernydiaz62@gmail.com>
2023-07-13 22:03:22 -0300

    Add: message when adding a product to the cart

--- frontend/src/app/shared/detalle-producto/detalle-producto.component.ts
+++ frontend/src/app/shared/detalle-producto/detalle-producto.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-detalle-producto',
   templateUrl: './detalle-producto.component.html',
   styleUrls: ['./detalle-producto.component.scss'],
 })
-export class DetalleProductoComponent {}
+export class DetalleProductoComponent {
+  messages!: Message[];
+
+  showQuantityProduc(product: any) {
+    console.log(product);
+    this.messages = [
+      {
+        severity: 'success',
+        summary: `${product.name}`,
+        detail: 'ha sido añadido exitosamente a tu carrito',
+      },
+    ];
+
+    setTimeout(() => {
+      this.messages = [];
+    }, 3000);
+  }
+}
