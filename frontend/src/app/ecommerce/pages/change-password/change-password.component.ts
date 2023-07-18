e3fd3194d0a77ee532ef0291f09910fba73d51de
jhonpulido1990 <3825@holbertonstudents.com>
2023-07-18 01:41:19 -0500

    change-password

--- frontend/src/app/ecommerce/pages/change-password/change-password.component.ts
+++ frontend/src/app/ecommerce/pages/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { matchpassword } from './matchpassword.validator';
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
+  contra_actual = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  new_pass = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  confir_pass = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
+  form = new FormGroup({
+    contra_actual: this.contra_actual,
+    new_pass: this.new_pass,
+    confir_pass: this.confir_pass
+  }, {
+    validators:matchpassword
+  })
+
+  password_send() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+    this.form.markAllAsTouched()
+  }
 }
