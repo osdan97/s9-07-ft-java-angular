61db04a70cd4e819a95c5cf76e502a5f3aab31ad
jhonpulido1990 <3825@holbertonstudents.com>
2023-07-18 09:54:09 -0500

    fixed validation confirm password

--- frontend/src/app/ecommerce/pages/change-password/change-password.component.ts
+++ frontend/src/app/ecommerce/pages/change-password/change-password.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { matchpassword } from './matchpassword.validator';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -17,8 +16,23 @@ export class ChangePasswordComponent {
     new_pass: this.new_pass,
     confir_pass: this.confir_pass
   }, {
-    validators:matchpassword
+    validators: [this.camposIguales('new_pass', 'confir_pass')],
   })
+
+  camposIguales(campo1: string, campo2: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const pass1 = formGroup.get(campo1)?.value;
+      const pass2 = formGroup.get(campo2)?.value;
+
+      if (pass1 !== pass2) {
+        formGroup.get(campo2)?.setErrors({ noIguales: true });
+        return { noIguales: true };
+      }
+
+      formGroup.get(campo2)?.setErrors(null);
+      return null;
+    };
+  }
 
   password_send() {
     if (this.form.valid) {
