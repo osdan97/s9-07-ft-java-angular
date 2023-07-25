9a5ee156c85e51f920c286682d6216fc3f04576b
jhonpulido1990 <3825@holbertonstudents.com>
2023-07-25 10:12:13 -0500

    fixed structure my-account form e info casa

--- frontend/src/app/shared/info-casa/info-casa.component.ts
+++ frontend/src/app/shared/info-casa/info-casa.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-info-casa',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-casa.component.scss']
 })
 export class InfoCasaComponent  {
+  @Output() salida = new EventEmitter<boolean>();
 
+  On_click() {
+    this.salida.emit(true);
+    console.log('true');
+  }
 }
