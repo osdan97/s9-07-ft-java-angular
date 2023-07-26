50e5bc9ee831a4af3bfd9bb70b912916d40bd5f2
LuMarka <luisamarkarian@gmail.com>
2023-07-26 06:05:17 -0300

    se comentan los AuthGuard en ecommerce-routing de legales, quienes somos, preguntas frecuentes y terminos y condiciones

--- frontend/src/app/ecommerce/ecommerce-routing.module.ts
+++ frontend/src/app/ecommerce/ecommerce-routing.module.ts
@@ -120,8 +120,8 @@ const routes: Routes = [
         path: 'legales',
         loadChildren: () =>
           import('./pages/legales/legales.module').then((m) => m.LegalesModule),
-        canActivate: [AuthGuard],
-        canMatch: [AuthGuard],
+       // canActivate: [AuthGuard],
+        //canMatch: [AuthGuard],
       },
       {
         path: 'preguntas-frecuentes',
@@ -129,8 +129,8 @@ const routes: Routes = [
           import(
             './pages/preguntas-frecuentes/preguntas-frecuentes.module'
           ).then((m) => m.PreguntasFrecuentesModule),
-        canActivate: [AuthGuard],
-        canMatch: [AuthGuard],
+       // canActivate: [AuthGuard],
+       // canMatch: [AuthGuard],
       },
 
       {
@@ -157,8 +157,8 @@ const routes: Routes = [
           import(
             './pages/terminos-condiciones/terminos-condiciones.module'
           ).then((m) => m.TerminosCondicionesModule),
-        canActivate: [AuthGuard],
-        canMatch: [AuthGuard],
+       // canActivate: [AuthGuard],
+       // canMatch: [AuthGuard],
       },
       {
         path: 'quienessomos',
@@ -166,8 +166,8 @@ const routes: Routes = [
           import('./pages/quienessomos/quienessomos.module').then(
             (m) => m.QuienessomosModule
           ),
-        canActivate: [AuthGuard],
-        canMatch: [AuthGuard],
+        //canActivate: [AuthGuard],
+        //canMatch: [AuthGuard],
       },
     ],
   },
