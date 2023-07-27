4965648650637b02560c4a91b3bef0c6adeafee1
Nirsch95 <castromanzano95@gmail.com>
2023-07-26 22:51:04 -0500

    Feat: adjust Pageable sort

--- backend/src/main/java/com/nocountry/ecommerce/controller/ProductController.java
+++ backend/src/main/java/com/nocountry/ecommerce/controller/ProductController.java
@@ -127,9 +127,6 @@ public class ProductController {
             if(page < 1){
                 return new ResponseEntity<>(new Mensaje("Page can't be less than 1"), HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            if(sort != null){
-                return new ResponseEntity<>(productService.getProducts(page, country, category, sort), HttpStatus.OK);
-            }
             return new ResponseEntity<>(productService.getProducts(page, country, category, sort), HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(new Mensaje(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
